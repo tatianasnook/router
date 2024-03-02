0fae6acb50a2f8468e2586f4e4de1ecf057c6194
tatianasnook <tatsnook@gmail.com>
2024-03-01 19:54:18 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+
+
+app.listen(4000, () => {
+  console.log('Listening on port 4000')
+})
+
+// //POSTS
+// app.get('/posts', (req, res) => {
+//   res.send('GET POSTS')
+// })
+
+// app.post('/posts', (req, res) => {
+//   res.send('POST POSTS')
+// })
+
+// app.put('/posts', (req, res) => {
+//   res.send('PUT POSTS')
+// })
+
+// app.delete('/posts', (req, res) => {
+//   res.send('DELETE POSTS')
+// })
+
+// //COMMENTS
+// app.get('/comments', (req, res) => {
+//   res.send('GET comments')
+// })
+
+// app.post('/comments', (req, res) => {
+//   res.send('POST comments')
+// })
+
+// app.put('/comments', (req, res) => {
+//   res.send('PUT comments')
+// })
+
+// app.delete('/comments', (req, res) => {
+//   res.send('DELETE comments')
+// })
+
+// LIKES
+
+// STORIES
+
